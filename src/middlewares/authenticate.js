1a2ec7359c31f1dfad93c745b12c89b563e0a9e4
pakornsukmankong <floksong.001@gmail.com>
2022-09-30 13:08:44 +0700

    multer + cloudinary

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,31 +1,32 @@
-const jwt = require('jsonwebtoken');
-const { User } = require('../models');
+const jwt = require('jsonwebtoken')
+const { User, Social, Blog } = require('../models')
 
-const AppError = require('../utils/appError');
+const AppError = require('../utils/appError')
 
 module.exports = async (req, res, next) => {
   try {
-    const { authorization } = req.headers;
+    const { authorization } = req.headers
     if (!authorization || !authorization.startsWith('Bearer')) {
-      throw new AppError('unauthenticated', 401);
+      throw new AppError('unauthenticated', 401)
     }
-    const token = authorization.split(' ')[1];
+    const token = authorization.split(' ')[1]
     if (!token) {
-      throw new AppError('unauthenticated', 401);
+      throw new AppError('unauthenticated', 401)
     }
-    const payload = jwt.verify(token, process.env.JWT_SECRET_KEY || 'private');
+    const payload = jwt.verify(token, process.env.JWT_SECRET_KEY || 'private')
 
     const user = await User.findOne({
       where: { id: payload.id },
       attributes: { exclude: 'password' },
-    });
+      include: [Social, Blog],
+    })
     if (!user) {
-      throw new AppError('unauthenticated', 401);
+      throw new AppError('unauthenticated', 401)
     }
 
-    req.user = user;
-    next();
+    req.user = user
+    next()
   } catch (err) {
-    next(err);
+    next(err)
   }
-};
+}
